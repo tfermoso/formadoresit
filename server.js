765faae7eb8751659806d8e3cce30612064ccb62
tfermoso <tomas.fermoso@gmail.com>
2022-11-08 17:08:26 +0100

    login1

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express')
 require('dotenv').config()
-
+var bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken');
 
 const app = express()
+app.use(bodyParser.json());
 
+app.post("/login",(req,res)=>{
+    if(comprobarUsuario(req.body.user,req.body.pass)){
+        //Envio Token
+        res.json({result:"ok"});
+    }else{
+        //Envío error
+        res.json({result:"Usuario o Contraseña no existe"});
+
+    }
+    
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Servidor iniciado en ${process.env.PORT}`)
+  
 })
+
+function comprobarUsuario(user,pass) {
+    if(user=="tomas" && pass=="1234"){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
